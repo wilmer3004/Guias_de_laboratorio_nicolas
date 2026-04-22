a3a88ea2ca93d6863c016f74262338885d81a11c
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2026-04-21 22:48:38 -0500

    Create Estudiante.java

---
+++ Guia5y6/Ejercicio2/Estudiante.java
@@ -0,0 +1,6 @@
+public class Estudiante extends Persona {
+
+    public Estudiante(String nombres, String apellidos, String direccion, String telefono, String carrera) {
+        super(nombres, apellidos, direccion, telefono, carrera);
+    }
+}
