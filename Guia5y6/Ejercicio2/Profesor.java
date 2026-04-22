8665266b7388ea87dd48909ea002b537ba9db957
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2026-04-21 22:48:21 -0500

    Create Profesor.java

---
+++ Guia5y6/Ejercicio2/Profesor.java
@@ -0,0 +1,6 @@
+public class Profesor extends Persona {
+
+    public Profesor(String nombres, String apellidos, String direccion, String telefono, String area) {
+        super(nombres, apellidos, direccion, telefono, area);
+    }
+}
