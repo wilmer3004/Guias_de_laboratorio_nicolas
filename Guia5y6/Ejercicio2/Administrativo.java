ff75ef865fbb97cd4f3a350b342fd4ae38371a73
wilmer300419 <131203913+wilmer3004@users.noreply.github.com>
2026-04-21 22:48:42 -0500

    Create Administrativo.java

---
+++ Guia5y6/Ejercicio2/Administrativo.java
@@ -0,0 +1,6 @@
+public class Administrativo extends Persona {
+
+    public Administrativo(String nombres, String apellidos, String direccion, String telefono, String area) {
+        super(nombres, apellidos, direccion, telefono, area);
+    }
+}
